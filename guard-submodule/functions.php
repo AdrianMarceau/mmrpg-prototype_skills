9ee5019c1c99a7bf76bc7311e3528d50168b3d60
Adrian Marceau <adrian.marceau@gmail.com>
2020-11-27 02:44:27 -0500

    Adjusted function code for guard/reverse submodule to work better w/ items of similar names

--- guard-submodule/functions.php
+++ guard-submodule/functions.php
@@ -3,26 +3,32 @@ $functions = array(
     'skill_function' => function($objects){
         return true;
     },
-    'rpg-ability_stat-boost_middle' => function($objects){
+    'rpg-ability_stat-boost_before' => function($objects){
         extract($objects);
         $options->return_early = true;
+        if (empty($options->boost_amount)){ return false; }
         if (!empty($this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'])){ return false; }
         else { $this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'] = true; }
+        if ($this_robot->has_item('reverse-module')){ $options->boost_amount *= -1; }
         $options->extra_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' prevents stat changes! ';
-        $options->extra_text .= '<br /> '.$this_robot->print_name().'\'s '.$options->stat_type.' was not raised!';
+        $options->extra_text .= '<br /> '.$this_robot->print_name().'\'s '.$options->stat_type.' was not '.($options->boost_amount > 0 ? 'raised' : 'lowered').'!';
         $this_skill->target_options_update(array('frame' => 'defend', 'success' => array(9, 0, 0, 10, $options->extra_text)));
         $this_robot->trigger_target($this_robot, $this_skill, array('prevent_default_text' => true));
+        $options->boost_amount = 0;
         return false;
     },
-    'rpg-ability_stat-break_middle' => function($objects){
+    'rpg-ability_stat-break_before' => function($objects){
         extract($objects);
         $options->return_early = true;
+        if (empty($options->break_amount)){ return false; }
         if (!empty($this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'])){ return false; }
         else { $this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'] = true; }
+        if ($this_robot->has_item('reverse-module')){ $options->break_amount *= -1; }
         $options->extra_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' protects against stat changes! ';
-        $options->extra_text .= '<br /> '.$this_robot->print_name().'\'s '.$options->stat_type.' was not lowered!';
+        $options->extra_text .= '<br /> '.$this_robot->print_name().'\'s '.$options->stat_type.' was not '.($options->break_amount > 0 ? 'lowered' : 'raised').'!';
         $this_skill->target_options_update(array('frame' => 'defend', 'success' => array(9, 0, 0, 10, $options->extra_text)));
         $this_robot->trigger_target($this_robot, $this_skill, array('prevent_default_text' => true));
+        $options->break_amount = 0;
         return false;
     }
 );
--- reverse-submodule/functions.php
+++ reverse-submodule/functions.php
@@ -7,9 +7,12 @@ $functions = array(
         extract($objects);
         if (!$options->is_fixed_amount){
             $options->return_early = true;
+            if ($this_robot->has_item('guard-module')){ $options->boost_amount *= -1; return false; }
             if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
             $options->extra_text .= $this_robot->print_name().'\'s '.$this_skill->print_name().' inverts stat changes! ';
-            return rpg_ability::ability_function_stat_break($this_robot, $options->stat_type, $options->boost_amount, $this_ability, $options->success_frame, $options->failure_frame, $options->extra_text, true);
+            $return_value = rpg_ability::ability_function_stat_break($this_robot, $options->stat_type, $options->boost_amount, $this_ability, $options->success_frame, $options->failure_frame, $options->extra_text, true);
+            $options->boost_amount *= -1;
+            return $return_value;
         } else {
             return true;
         }
@@ -18,9 +21,12 @@ $functions = array(
         extract($objects);
         if (!$options->is_fixed_amount){
             $options->return_early = true;
+            if ($this_robot->has_item('guard-module')){ $options->break_amount *= -1; return false; }
             if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
             $options->extra_text .= $this_robot->print_name().'\'s '.$this_skill->print_name().' inverts stat changes! ';
-            return rpg_ability::ability_function_stat_boost($this_robot, $options->stat_type, $options->break_amount, $this_ability, $options->success_frame, $options->failure_frame, $options->extra_text, true);
+            $return_value = rpg_ability::ability_function_stat_boost($this_robot, $options->stat_type, $options->break_amount, $this_ability, $options->success_frame, $options->failure_frame, $options->extra_text, true);
+            $options->break_amount *= -1;
+            return $return_value;
         } else {
             return true;
         }
