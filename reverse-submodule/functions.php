0b60cb2bedec691cf54ebe96003d788b06190607
Ageman20XX <adrian.marceau@gmail.com>
2020-11-26 23:49:17 -0500

    Updated Reverse Submodule's data and functions

---
+++ reverse-submodule/functions.php
@@ -0,0 +1,29 @@
+<?
+$functions = array(
+    'skill_function' => function($objects){
+        return true;
+    },
+    'rpg-ability_stat-boost_before' => function($objects){
+        extract($objects);
+        if (!$options->is_fixed_amount){
+            $options->return_early = true;
+            if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
+            $options->extra_text .= $this_robot->print_name().'\'s '.$this_skill->print_name().' inverts stat changes! ';
+            return rpg_ability::ability_function_stat_break($this_robot, $options->stat_type, $options->boost_amount, $this_ability, $options->success_frame, $options->failure_frame, $options->extra_text, true);
+        } else {
+            return true;
+        }
+    },
+    'rpg-ability_stat-break_before' => function($objects){
+        extract($objects);
+        if (!$options->is_fixed_amount){
+            $options->return_early = true;
+            if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
+            $options->extra_text .= $this_robot->print_name().'\'s '.$this_skill->print_name().' inverts stat changes! ';
+            return rpg_ability::ability_function_stat_boost($this_robot, $options->stat_type, $options->break_amount, $this_ability, $options->success_frame, $options->failure_frame, $options->extra_text, true);
+        } else {
+            return true;
+        }
+    }
+);
+?>
