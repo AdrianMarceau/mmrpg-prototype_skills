aab16f434692fafe5913eb3c2bc279be17270d93
Ageman20XX <adrian.marceau@gmail.com>
2023-06-23 01:30:21 -0400

    Updated Guard Submodule's functions

--- guard-submodule/functions.php
+++ guard-submodule/functions.php
@@ -16,10 +16,21 @@ $functions = array(
         if (!empty($this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'])){ return false; }
         else { $this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'] = true; }
         if ($this_robot->has_item('reverse-module')){ $options->boost_amount *= -1; }
+        $options->header_text = $this_robot->robot_name.'\'s '.$this_skill->skill_name;
         $options->extra_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' prevents stat changes! ';
         $options->extra_text .= '<br /> '.$this_robot->print_name().'\'s '.$options->stat_type.' was not '.($options->boost_amount > 0 ? 'raised' : 'lowered').'!';
-        $this_skill->target_options_update(array('frame' => 'defend', 'success' => array(9, 0, 0, 10, $options->extra_text)));
-        $this_robot->trigger_target($this_robot, $this_skill, array('prevent_default_text' => true));
+        $this_robot->set_frame('defend');
+        $this_battle->events_create($this_robot, false, $options->header_text, $options->extra_text, array(
+                'this_skill' => $this_skill,
+                'canvas_show_this_skill_overlay' => false,
+                'canvas_show_this_skill_underlay' => true,
+                'event_flag_camera_action' => true,
+                'event_flag_camera_side' => $this_robot->player->player_side,
+                'event_flag_camera_focus' => $this_robot->robot_position,
+                'event_flag_camera_depth' => $this_robot->robot_key
+                )
+            );
+        $this_robot->reset_frame();
         $options->boost_amount = 0;
         $options->extra_text = '';
         return false;
@@ -32,10 +43,21 @@ $functions = array(
         if (!empty($this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'])){ return false; }
         else { $this_skill->skill_results['flag_'.$this_skill->skill_token.'_triggered'] = true; }
         if ($this_robot->has_item('reverse-module')){ $options->break_amount *= -1; }
+        $options->header_text = $this_robot->print_name().'\'s '.$this_skill->print_name();
         $options->extra_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' protects against stat changes! ';
         $options->extra_text .= '<br /> '.$this_robot->print_name().'\'s '.$options->stat_type.' was not '.($options->break_amount > 0 ? 'lowered' : 'raised').'!';
-        $this_skill->target_options_update(array('frame' => 'defend', 'success' => array(9, 0, 0, 10, $options->extra_text)));
-        $this_robot->trigger_target($this_robot, $this_skill, array('prevent_default_text' => true));
+        $this_robot->set_frame('defend');
+        $this_battle->events_create($this_robot, false, $options->header_text, $options->extra_text, array(
+                'this_skill' => $this_skill,
+                'canvas_show_this_skill_overlay' => false,
+                'canvas_show_this_skill_underlay' => true,
+                'event_flag_camera_action' => true,
+                'event_flag_camera_side' => $this_robot->player->player_side,
+                'event_flag_camera_focus' => $this_robot->robot_position,
+                'event_flag_camera_depth' => $this_robot->robot_key
+                )
+            );
+        $this_robot->reset_frame();
         $options->break_amount = 0;
         $options->extra_text = '';
         return false;
