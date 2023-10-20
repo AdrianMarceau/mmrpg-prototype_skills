f63d6a0d6b856c98639b992a8654742d40ffa866
Ageman20XX <adrian.marceau@gmail.com>
2023-10-20 11:42:11 -0400

    Updated Disarmament Submodule's data and functions

--- disarmament-submodule/functions.php
+++ disarmament-submodule/functions.php
@@ -23,63 +23,159 @@ $functions = array(
             'galaxy-bomb_black-hole',  // battle_attachments/side-position/ability_galaxy-bomb_black-hole_side-position
             'crash-bomber_crash-bomb',  // robot_attachments//ability_crash-bomber_crash-bomb
             'chain-blast_chain-bomb',  // robot_attachments//ability_chain-blast_chain-bomb
-                'chain-blast_fx',  // robot_attachments//ability_chain-blast_fx
             );
+        $object_fx_tokens = array(
+            'chain-blast_chain-bomb' => 'chain-blast_fx'
+            );
+
+        // Create an array to hold positions of special interest (hazards to remove)
+        $positions_of_interest = array();
+
+        // Collect a list of active robots from this player so we can loop through 'em
+        $this_player_robots = $this_player->values['robots_active'];
+        //error_log('$this_player_robots = '.print_r($this_player_robots, true));
 
         // Loop through field attachments on this side of the battle first
         if (!empty($this_battle->battle_attachments)){
             foreach ($this_battle->battle_attachments AS $side_position => $battle_attachments){
                 if (!strstr($side_position, $this_player->player_side.'-')){ continue; }
                 //error_log('$battle_attachments = '.print_r($battle_attachments, true));
+                //error_log('$battle_attachments = '.print_r(array_keys($battle_attachments), true));
+                $attachment_token_regex = '/^ability_([-_a-z0-9]+)_([-a-z0-9]+)$/';
                 foreach ($battle_attachments AS $attachment_token => $attachment_info){
-                    $attachment_token_clean = preg_replace('/^ability_([-_a-z0-9]+)_(?:[-a-z0-9]+)$/', '$1', $attachment_token);
+                    $attachment_token_clean = preg_replace($attachment_token_regex, '$1', $attachment_token);
+                    $attachment_token_context = preg_replace($attachment_token_regex, '$2', $attachment_token);
                     if (!in_array($attachment_token_clean, $explosive_object_tokens)){ continue; }
-                    $explosive_objects_removed++;
-                    $this_battle->unset_attachment($side_position, $attachment_token);
+                    $attachment_fx_token = false;
+                    if (isset($object_fx_tokens[$attachment_token_clean])){
+                        $attachment_fx_token = str_replace(
+                            $attachment_token_clean,
+                            $object_fx_tokens[$attachment_token_clean],
+                            $attachment_token
+                            );
+                        }
+                    if (strstr($side_position, 'bench-')){ list($side, $position, $key) = explode('-', $attachment_token_context); }
+                    else { list($side, $position) = explode('-', $attachment_token_context); $key = 0; }
+                    //error_log('$attachment_token_clean = '.print_r($attachment_token_clean, true));
+                    //error_log('$attachment_token_context = '.print_r($attachment_token_context, true));
+                    //error_log('looking for '.$side.' '.$position.' '.$key.' in $this_player_robots');
+                    $robot = array_filter($this_player_robots, function($info) use ($side, $position, $key){
+                        if ($info['robot_position'] !== $position){ return false; }
+                        if ($position === 'bench' && $info['robot_key'] !== $key){ return false; }
+                        return true;
+                        });
+                    if (!empty($robot)){ $robot = rpg_game::get_robot_by_id($robot[0]['robot_id']); }
+                    //error_log('$robot = '.print_r($robot, true));
+                    //error_log('$robot = '.print_r(($robot ? $robot->robot_string : 'false'), true));
+                    $positions_of_interest[] = array(
+                        'kind' => 'battle',
+                        'key' => $side_position,
+                        'token' => $attachment_token,
+                        'fxtoken' => $attachment_fx_token,
+                        'player' => $this_player,
+                        'robot' => (!empty($robot) ? $robot : $this_robot),
+                        );
                 }
             }
         }
 
         // Collect a list of robots from this player and then loop through 'em
-        $this_player_robots = $this_player->values['robots_active'];
         if (!empty($this_player_robots)){
             foreach ($this_player_robots AS $robot_key => $robot_info){
-                if ($robot_info['robot_id'] === $this_robot->robot_id){ $this_player_robot = $this_robot; }
-                else { $this_player_robot = rpg_game::get_robot($this_battle, $this_player, array('robot_id' => $robot_info['robot_id'])); }
-                if (empty($this_player_robot)){ unset($this_player_robot); continue; }
+                if ($robot_info['robot_id'] === $this_robot->robot_id){ $robot = $this_robot; }
+                else { $robot = rpg_game::get_robot($this_battle, $this_player, array('robot_id' => $robot_info['robot_id'])); }
+                if (empty($robot)){ unset($robot); continue; }
                 // Loop through this robot's attachments
-                if (!empty($this_player_robot->robot_attachments)){
-                    foreach ($this_player_robot->robot_attachments AS $attachment_token => $attachment_info){
+                if (!empty($robot->robot_attachments)){
+                    foreach ($robot->robot_attachments AS $attachment_token => $attachment_info){
                         $attachment_token_clean = preg_replace('/^ability_([-_a-z0-9]+)$/', '$1', $attachment_token);
                         if (!in_array($attachment_token_clean, $explosive_object_tokens)){ continue; }
-                        $explosive_objects_removed++;
-                        $this_player_robot->unset_attachment($attachment_token);
+                        $attachment_fx_token = false;
+                        if (isset($object_fx_tokens[$attachment_token_clean])){
+                            $attachment_fx_token = str_replace(
+                                $attachment_token_clean,
+                                $object_fx_tokens[$attachment_token_clean],
+                                $attachment_token
+                                );
+                            }
+                        $positions_of_interest[] = array(
+                            'kind' => 'robot',
+                            'token' => $attachment_token,
+                            'fxtoken' => $attachment_fx_token,
+                            'player' => $this_player,
+                            'robot' => $robot,
+                            );
                     }
                 }
-                unset($this_player_robot);
+                unset($robot);
             }
         }
 
-        // Check to see if any explosives were removed this way
-        if (!empty($explosive_objects_removed)){
+        //error_log('$positions_of_interest = '.print_r(array_keys($positions_of_interest), true));
+        //error_log('$positions_of_interest = '.print_r($positions_of_interest, true));
+
+        // If there aren't any points of interest, we should return now
+        if (empty($positions_of_interest)){ return false; }
+
+        // Otherwise, print a message showing that this effect is taking place
+        $certain_text = count($positions_of_interest) === 1 ? 'a hazard' : 'some hazards';
+        $this_robot->set_frame('taunt');
+        $this_battle->queue_sound_effect('scan-start');
+        $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name,
+            $this_robot->print_name().' took notice of '.$certain_text.' on the field...',
+            array(
+                'event_flag_camera_action' => true,
+                'event_flag_camera_side' => $this_robot->player->player_side,
+                'event_flag_camera_focus' => $this_robot->robot_position,
+                'event_flag_camera_depth' => $this_robot->robot_key
+                )
+            );
+        $this_robot->reset_frame();
+
+        // Loop through and show the camera looking at them one-by-one
+        foreach ($positions_of_interest AS $key => $hazard_to_remove){
+
+            // Show the camera looking at this robot first
+            $hazard_to_remove['robot']->set_frame('defend');
+            $this_battle->queue_sound_effect('scan-start');
+            $this_battle->events_create($this_robot, false, '', '',
+                array(
+                    'event_flag_camera_action' => true,
+                    'event_flag_camera_side' => $hazard_to_remove['robot']->player->player_side,
+                    'event_flag_camera_focus' => $hazard_to_remove['robot']->robot_position,
+                    'event_flag_camera_depth' => $hazard_to_remove['robot']->robot_key
+                    )
+                );
+            $hazard_to_remove['robot']->reset_frame();
+
+            // And now we can actually remove the hazard while showing the skill popup for each
+            if ($hazard_to_remove['kind'] === 'battle'){
+                $this_battle->unset_attachment($hazard_to_remove['key'], $hazard_to_remove['token']);
+                if (!empty($hazard_to_remove['fxtoken'])){ $this_battle->unset_attachment($hazard_to_remove['key'], $hazard_to_remove['fxtoken']); }
+            } else if ($hazard_to_remove['kind'] === 'robot'){
+                $hazard_to_remove['robot']->unset_attachment($hazard_to_remove['token']);
+                if (!empty($hazard_to_remove['fxtoken'])){ $hazard_to_remove['robot']->unset_attachment($hazard_to_remove['fxtoken']); }
+            }
 
             // Print a message showing that this effect is taking place
+            $hazard_to_remove['robot']->set_frame('taunt');
             $this_robot->set_frame('taunt');
             $this_battle->queue_sound_effect('debuff-received');
             $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name,
                 $this_robot->print_name().'\'s '.$this_skill->print_name().' skill kicked in!<br />'.
-                'Explosive hazards on '.$this_robot->get_pronoun('possessive2').' side of the field have been disarmed!',
+                'The explosive hazard threatening '.$hazard_to_remove['robot']->print_name().' was disarmed!',
                 array(
                     'this_skill' => $this_skill,
                     'canvas_show_this_skill_overlay' => false,
                     'canvas_show_this_skill_underlay' => true,
                     'event_flag_camera_action' => true,
-                    'event_flag_camera_side' => $this_robot->player->player_side,
-                    'event_flag_camera_focus' => $this_robot->robot_position,
-                    'event_flag_camera_depth' => $this_robot->robot_key
+                    'event_flag_camera_side' => $hazard_to_remove['robot']->player->player_side,
+                    'event_flag_camera_focus' => $hazard_to_remove['robot']->robot_position,
+                    'event_flag_camera_depth' => $hazard_to_remove['robot']->robot_key
                     )
                 );
             $this_robot->reset_frame();
+            $hazard_to_remove['robot']->reset_frame();
 
         }
 
