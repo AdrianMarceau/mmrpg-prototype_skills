eaf529589f4fd53496a33123bf16637ed9bb42da
Ageman20XX <adrian.marceau@gmail.com>
2023-10-25 14:09:48 -0400

    Updated Anti-Speed Submodule's functions

--- anti-speed-submodule/functions.php
+++ anti-speed-submodule/functions.php
@@ -20,30 +20,6 @@ $functions = array(
         // Create an array to hold positions of special interest (stats to reset)
         $positions_of_interest = array();
 
-        // Collect a list of active robots from the target player so we can loop through 'em
-        $target_player_robots = $target_player->values['robots_active'];
-        if (!isset($target_robot)){ $target_robot = $target_player->get_active_robot(); }
-
-        // Collect a list of robots from this player and then loop through 'em
-        if (!empty($target_player_robots)){
-            foreach ($target_player_robots AS $robot_key => $robot_info){
-                if ($robot_info['robot_id'] === $target_robot->robot_id){ $robot = $target_robot; }
-                else { $robot = rpg_game::get_robot($this_battle, $target_player, array('robot_id' => $robot_info['robot_id'])); }
-                if (empty($robot)){ unset($robot); continue; }
-                // Check to see if this robot's stat has been modified
-                $anti_stat_value = isset($robot->counters[$anti_stat_token.'_mods']) ? $robot->counters[$anti_stat_token.'_mods'] : 0;
-                if ($anti_stat_value > 0 || $anti_stat_value < 0){
-                    $positions_of_interest[] = array(
-                        'stat' => $anti_stat_token,
-                        'value' => $anti_stat_value,
-                        'player' => $target_player,
-                        'robot' => $robot,
-                        );
-                }
-                unset($robot);
-            }
-        }
-
         // Collect a list of active robots from this player so we can loop through 'em
         $this_player_robot = $this_player->values['robots_active'];
         if (!isset($this_robot)){ $this_robot = $this_player->get_active_robot(); }
@@ -56,13 +32,46 @@ $functions = array(
                 if (empty($robot)){ unset($robot); continue; }
                 // Check to see if this robot's stat has been modified
                 $anti_stat_value = isset($robot->counters[$anti_stat_token.'_mods']) ? $robot->counters[$anti_stat_token.'_mods'] : 0;
-                if ($anti_stat_value > 0 || $anti_stat_value < 0){
+                $holding_anti_stat_booster = $robot->robot_item === $anti_stat_token.'-booster' ? true : false;
+                $anti_stat_item = $robot->has_item() && $holding_anti_stat_booster ? true : false;
+                if ($anti_stat_value > 0 || $anti_stat_value < 0 || $anti_stat_item){
                     $positions_of_interest[] = array(
                         'stat' => $anti_stat_token,
                         'value' => $anti_stat_value,
                         'player' => $this_player,
                         'robot' => $robot,
                         );
+                } elseif ($holding_anti_stat_booster) {
+                    $robot->set_counter('item_disabled', 3);
+                }
+                unset($robot);
+            }
+        }
+
+        // Collect a list of active robots from the target player so we can loop through 'em
+        $target_player_robots = $target_player->values['robots_active'];
+        if (!isset($target_robot)){ $target_robot = $target_player->get_active_robot(); }
+
+        // Collect a list of robots from this player and then loop through 'em
+        if (!empty($target_player_robots)){
+            foreach ($target_player_robots AS $robot_key => $robot_info){
+                if ($robot_info['robot_id'] === $target_robot->robot_id){ $robot = $target_robot; }
+                else { $robot = rpg_game::get_robot($this_battle, $target_player, array('robot_id' => $robot_info['robot_id'])); }
+                if (empty($robot)){ unset($robot); continue; }
+                // Check to see if this robot's stat has been modified
+                $anti_stat_value = isset($robot->counters[$anti_stat_token.'_mods']) ? $robot->counters[$anti_stat_token.'_mods'] : 0;
+                $holding_anti_stat_booster = $robot->robot_item === $anti_stat_token.'-booster' ? true : false;
+                $holding_anti_stat_booster = $robot->robot_item === $anti_stat_token.'-booster' ? true : false;
+                $anti_stat_item = $robot->has_item() && $holding_anti_stat_booster ? true : false;
+                if ($anti_stat_value > 0 || $anti_stat_value < 0 || $anti_stat_item){
+                    $positions_of_interest[] = array(
+                        'stat' => $anti_stat_token,
+                        'value' => $anti_stat_value,
+                        'player' => $target_player,
+                        'robot' => $robot,
+                        );
+                } elseif ($holding_anti_stat_booster) {
+                    $robot->set_counter('item_disabled', 3);
                 }
                 unset($robot);
             }
@@ -76,7 +85,7 @@ $functions = array(
         $this_robot->set_frame('taunt');
         $this_battle->queue_sound_effect('scan-start');
         $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name,
-            $this_robot->print_name().' took notice of '.$certain_text.' on the field...',
+            $this_robot->print_name().' took notice of '.$certain_text.' on the battlefield...',
             array(
                 'this_skill' => $this_skill,
                 'canvas_show_this_skill_overlay' => false,
@@ -108,23 +117,63 @@ $functions = array(
                 );
             $stat_to_remove['robot']->reset_frame();
 
-            // If this robot has a stat-based skill, display the trigger text separately
-            $trigger_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' skill kicked in!';
-            if (!empty($this_robot->robot_item) && preg_match('/^(guard|reverse|xtreme)-module$/', $this_robot->robot_item)){
-                $this_skill->target_options_update(array('frame' => 'summon', 'success' => array(9, 0, 0, -10, $trigger_text)));
-                $this_robot->trigger_target($this_robot, $this_skill, array('prevent_default_text' => true));
-                $trigger_text = '';
+            // Only reset stats if there are stats to reset (otherwise this is about an item)
+            $already_kicked_in = false;
+            if (!empty($stat_to_remove['value'])){
+
+                // If this robot has a stat-based skill, display the trigger text separately
+                $already_kicked_in = true;
+                $trigger_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' skill kicked in! ';
+                if ($this_robot->has_item() && preg_match('/^(guard|reverse|xtreme)-module$/', $this_robot->robot_item)){
+                    $this_skill->target_options_update(array('frame' => 'summon', 'success' => array(9, 0, 0, -10, $trigger_text)));
+                    $this_robot->trigger_target($this_robot, $this_skill, array('prevent_default_text' => true));
+                    $trigger_text = '';
+                }
+                // And now we can actually remove the stat change while showing the skill popup for each
+                $this_robot->set_frame('taunt');
+                //$this_battle->queue_sound_effect('debuff-received');
+                rpg_ability::ability_function_stat_reset($stat_to_remove['robot'], $stat_to_remove['stat'], $this_skill, array(
+                    'initiator_robot' => $this_robot,
+                    'success_frame' => 9,
+                    'failure_frame' => 9,
+                    'extra_text' => $trigger_text
+                    ));
+                $this_robot->reset_frame();
+
             }
-            // And now we can actually remove the stat change while showing the skill popup for each
-            $this_robot->set_frame('taunt');
-            //$this_battle->queue_sound_effect('debuff-received');
-            rpg_ability::ability_function_stat_reset($stat_to_remove['robot'], $stat_to_remove['stat'], $this_skill, array(
-                'initiator_robot' => $this_robot,
-                'success_frame' => 9,
-                'failure_frame' => 9,
-                'extra_text' => $trigger_text
-                ));
-            $this_robot->reset_frame();
+
+            // Regardless, if the target was holding a relevant booster module, disable it until end of turn
+            if ($stat_to_remove['robot']->has_item()
+                && $stat_to_remove['robot']->robot_item === $stat_to_remove['stat'].'-booster'){
+
+                // Disable the item for the rest of the turn and a few more
+                $stat_to_remove['robot']->set_counter('item_disabled', 3);
+
+                // Otherwise, print a message showing that this effect is taking place
+                $this_robot->set_frame('taunt');
+                $this_battle->queue_sound_effect('small-buff-received');
+                $trigger_options = array(
+                    'this_skill' => $this_skill,
+                    'canvas_show_this_skill_overlay' => false,
+                    'canvas_show_this_skill_underlay' => true,
+                    'event_flag_camera_action' => true,
+                    'event_flag_camera_side' => $stat_to_remove['robot']->player->player_side,
+                    'event_flag_camera_focus' => $stat_to_remove['robot']->robot_position,
+                    'event_flag_camera_depth' => $stat_to_remove['robot']->robot_key
+                    );
+                if (!$already_kicked_in){
+                    $already_kicked_in = true;
+                    $trigger_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' skill kicked in! ';
+                    $trigger_text .= '<br />'.$stat_to_remove['robot']->print_name().'\'s item was preemptively disabled!';
+                } else {
+                    $trigger_text = 'The skill prevents future disturbances too... ';
+                    $trigger_text .= '<br />'.$stat_to_remove['robot']->print_name().'\'s item was disabled!';
+                    $trigger_options['canvas_show_this_skill'] = false;
+                }
+                $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name, $trigger_text, $trigger_options);
+                $this_robot->reset_frame();
+
+            }
 
         }
 
