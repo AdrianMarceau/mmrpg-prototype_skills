9af6d32666ed8247bec830e85c702218f32d541a
Ageman20XX <adrian.marceau@gmail.com>
2023-11-16 11:27:50 -0500

    Updated Hyperscan Submodule's functions

--- hyperscan-submodule/functions.php
+++ hyperscan-submodule/functions.php
@@ -10,13 +10,18 @@ $functions = array(
 
         // Turn ON the ability to see skills/abilities during the scan
         // by adding this robot's ID to the player's hyperscan list
+        $show_hyperscan = false;
         $hyperscan_robots = $this_player->get_value('hyperscan_robots');
         if (empty($hyperscan_robots)){ $hyperscan_robots = array(); }
-        if (!in_array($this_robot->robot_id, $hyperscan_robots)){ $hyperscan_robots[] = $this_robot->robot_id; }
+        if (!in_array($this_robot->robot_id, $hyperscan_robots)){
+            $hyperscan_robots[] = $this_robot->robot_id;
+            $show_hyperscan = true;
+        }
         $this_player->set_value('hyperscan_robots', $hyperscan_robots);
 
         // Only bother printing this message if the player is a human
-        if ($this_player->player_autopilot === false){
+        if ($show_hyperscan
+            && $this_player->player_autopilot === false){
             // Print a message showing that this effect is taking place
             $this_robot->set_frame('taunt');
             $this_battle->queue_sound_effect('scan-start');
@@ -39,6 +44,34 @@ $functions = array(
         // Return true on success
         return true;
 
+    },
+    'rpg-robot_trigger-disabled_after' => function($objects){
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // If this robot is not the target, then we can return early
+        if ($this_robot !== $options->disabled_target){ return false; }
+
+        // Turn OFF the ability to see skills/abilities during the scan
+        // by removing this robot's ID to the player's hyperscan list
+        $hyperscan_robots = $this_player->get_value('hyperscan_robots');
+        if (empty($hyperscan_robots)){ $hyperscan_robots = array(); }
+        if (in_array($this_robot->robot_id, $hyperscan_robots)){ $hyperscan_robots = array_diff($hyperscan_robots, array($this_robot->robot_id)); }
+        $this_player->set_value('hyperscan_robots', $hyperscan_robots);
+
+        // Return true on success
+        return true;
+
     }
 );
+$functions['rpg-robot_check-skills_turn-start'] = function($objects) use ($functions){
+    return $functions['rpg-robot_check-skills_battle-start']($objects, false);
+};
+$functions['rpg-robot_check-skills_end-of-turn'] = function($objects) use ($functions){
+    return $functions['rpg-robot_check-skills_battle-start']($objects, false);
+};
+$functions['rpg-battle_switch-in_after'] = function($objects) use ($functions){
+    return $functions['rpg-robot_check-skills_battle-start']($objects, false);
+};
 ?>
