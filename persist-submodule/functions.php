f386405f562a2560dce1b606f5164fe1225fd362
Ageman20XX <adrian.marceau@gmail.com>
2023-11-17 02:48:12 -0500

    Updated Persist Submodule's functions

--- persist-submodule/functions.php
+++ persist-submodule/functions.php
@@ -3,21 +3,31 @@ $functions = array(
     'skill_function' => function($objects){
         return true;
     },
-    'rpg-ability_trigger-damage_before' => function($objects){
-        //error_log('rpg-ability_trigger-damage_before() for '.$objects['this_robot']->robot_string.'//'.$objects['this_skill']->skill_token);
+    'rpg-robot_check-skills_pre-check-health' => function($objects){
+        //error_log('rpg-robot_check-skills_pre-check-health() for '.$objects['this_robot']->robot_string.'//'.$objects['this_skill']->skill_token);
 
-        // Extract objects into the global scope
+        // Extract all objects into the current scope
         extract($objects);
-
-        // If this robot is not the recipient, the skill doesn't activate
-        if ($options->damage_target !== $this_robot){ return false; }
-        //if (empty($options->damage_target)){ return false; }
-        //$target_robot = $options->damage_target;
 
         // Take note of whether or not this robot is starting with full health
         $full_health_before_damage = $this_robot->robot_energy >= $this_robot->robot_base_energy ? true : false;
         $this_robot->set_flag('full_health_before_damage', $full_health_before_damage);
+        //error_log('$this_robot->robot_energy: '.print_r($this_robot->robot_energy, true));
+        //error_log('$this_robot->robot_base_energy: '.print_r($this_robot->robot_base_energy, true));
         //error_log('full_health_before_damage: '.($full_health_before_damage ? 'true' : 'false'));
+
+        // Return true on success
+        return true;
+
+    },
+    'rpg-robot_check-skills_end-of-turn' => function($objects){
+        //error_log('rpg-robot_check-skills_end-of-turn() for '.$objects['this_robot']->robot_string.'//'.$objects['this_skill']->skill_token);
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Unset the full health check flag now that we're done with it
+        $this_robot->unset_flag('full_health_before_damage');
 
         // Return true on success
         return true;
@@ -39,13 +49,13 @@ $functions = array(
         // Check to see if this robot had full health before the damage
         $full_health_before_damage = $this_robot->get_flag('full_health_before_damage');
         //error_log('full_health_before_damage: '.($full_health_before_damage ? 'true' : 'false'));
-        $this_robot->unset_flag('full_health_before_damage');
 
         // Check to see if this robot is now at zero energy and thus would be disabled
         $zero_health_after_damage = $this_robot->robot_energy <= 0 ? true : false;
         //error_log('$zero_health_after_damage: '.($zero_health_after_damage ? 'true' : 'false'));
 
         // Check to see if this was a legit OHKO from full health otherwise the skill doesn't activate
+        $this_robot->set_flag('full_health_before_damage', false);
         if (!$full_health_before_damage || !$zero_health_after_damage){ return false; }
 
         // Revive this robot with the appropriate energy and remove any flags
@@ -77,6 +87,15 @@ $functions = array(
         // Return true on success
         return true;
 
-    },
+    }
 );
+$functions['rpg-robot_check-skills_battle-start'] = function($objects) use ($functions){
+    return $functions['rpg-robot_check-skills_pre-check-health']($objects, true);
+};
+$functions['rpg-robot_check-skills_turn-start'] = function($objects) use ($functions){
+    return $functions['rpg-robot_check-skills_pre-check-health']($objects, true);
+};
+$functions['rpg-battle_switch-in_after'] = function($objects) use ($functions){
+    return $functions['rpg-robot_check-skills_pre-check-health']($objects, false);
+};
 ?>
