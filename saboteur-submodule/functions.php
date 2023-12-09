c0762370b4d93a23cbab2cdbd045f2c7b3311ee7
Ageman20XX <adrian.marceau@gmail.com>
2023-12-09 02:29:27 -0500

    Updated Saboteur Submodule's functions

--- saboteur-submodule/functions.php
+++ saboteur-submodule/functions.php
@@ -8,30 +8,49 @@ $functions = array(
         // Extract all objects into the current scope
         extract($objects);
 
-        // Turn ON the priority-blocking feature of this skill
+        // Check to see if this robot's skill is currently active
+        $skill_is_active = false;
+        if ($this_robot->robot_status !== 'disabled'
+            && $this_robot->robot_position === 'active'){
+            $skill_is_active = true;
+        }
+
+        // Turn ON the heal-blocking feature of this skill
         // by adding this robot's ID to the player's anti_recovery list
+        // and turn it OFF by removing it from the list
+        $anti_recovery_added = false;
         $anti_recovery_robots = $this_player->get_value('anti_recovery_robots');
         if (empty($anti_recovery_robots)){ $anti_recovery_robots = array(); }
-        if (!in_array($this_robot->robot_id, $anti_recovery_robots)){ $anti_recovery_robots[] = $this_robot->robot_id; }
+        if ($skill_is_active && !in_array($this_robot->robot_id, $anti_recovery_robots)){
+            $anti_recovery_robots[] = $this_robot->robot_id;
+            $anti_recovery_added = true;
+        } elseif (!$skill_is_active && in_array($this_robot->robot_id, $anti_recovery_robots)){
+            $anti_recovery_robots = array_diff($anti_recovery_robots, array($this_robot->robot_id));
+        }
         $this_player->set_value('anti_recovery_robots', $anti_recovery_robots);
 
+        // If the skill isn't active don't show anthing
+        if (!$skill_is_active){ return false; }
+
         // Print a message showing that this effect is taking place
-        $this_robot->set_frame('taunt');
-        $this_battle->queue_sound_effect('scan-start');
-        $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name,
-            $this_robot->print_name().'\'s '.$this_skill->print_name().' skill kicked in!<br />'.
-            'Recovery abilities are useless as long as this robot remains active!',
-            array(
-                'this_skill' => $this_skill,
-                'canvas_show_this_skill_overlay' => false,
-                'canvas_show_this_skill_underlay' => true,
-                'event_flag_camera_action' => true,
-                'event_flag_camera_side' => $this_robot->player->player_side,
-                'event_flag_camera_focus' => $this_robot->robot_position,
-                'event_flag_camera_depth' => $this_robot->robot_key
-                )
-            );
-        $this_robot->reset_frame();
+        if ($anti_recovery_added){
+            $this_robot->set_frame('taunt');
+            $this_battle->queue_sound_effect('vector-sound');
+            $this_battle->events_create($this_robot, false, $this_robot->robot_name.'\'s '.$this_skill->skill_name,
+                $this_robot->print_name().'\'s '.$this_skill->print_name().' skill kicked in!<br />'.
+                'Recovery abilities are useless as long as this robot remains active!',
+                array(
+                    'this_skill' => $this_skill,
+                    'canvas_show_this_skill_overlay' => false,
+                    'canvas_show_this_skill_underlay' => true,
+                    'event_flag_camera_action' => true,
+                    'event_flag_camera_side' => $this_robot->player->player_side,
+                    'event_flag_camera_focus' => $this_robot->robot_position,
+                    'event_flag_camera_depth' => $this_robot->robot_key
+                    )
+                );
+            $this_robot->reset_frame();
+        }
 
         // Return true on success
         return true;
@@ -56,14 +75,34 @@ $functions = array(
         return true;
 
     },
+    'rpg-skill_disable-skill_before' => function($objects){
+        //error_log('rpg-skill_disable-skill_before() for '.$objects['this_robot']->robot_string);
+
+        // Extract all objects into the current scope
+        extract($objects);
+
+        // Turn OFF the heal-blocking feature of this skill by removing it from the list
+        $anti_recovery_removed = false;
+        $anti_recovery_robots = $this_player->get_value('anti_recovery_robots');
+        if (empty($anti_recovery_robots)){ $anti_recovery_robots = array(); }
+        if (in_array($this_robot->robot_id, $anti_recovery_robots)){
+            $anti_recovery_robots = array_diff($anti_recovery_robots, array($this_robot->robot_id));
+            $anti_recovery_removed = true;
+        }
+        $this_player->set_value('anti_recovery_robots', $anti_recovery_robots);
+
+        // Return true on success
+        return true;
+
+    }
 );
 $functions['rpg-robot_check-skills_battle-start'] = function($objects) use ($functions){
-    return $functions['rpg-robot_check-skills_update-saboteurs']($objects, true);
+    return $functions['rpg-robot_check-skills_update-saboteurs']($objects);
 };
 $functions['rpg-robot_check-skills_turn-start'] = function($objects) use ($functions){
-    return $functions['rpg-robot_check-skills_update-saboteurs']($objects, true);
+    return $functions['rpg-robot_check-skills_update-saboteurs']($objects);
 };
 $functions['rpg-battle_switch-in_after'] = function($objects) use ($functions){
-    return $functions['rpg-robot_check-skills_update-saboteurs']($objects, false);
+    return $functions['rpg-robot_check-skills_update-saboteurs']($objects);
 };
 ?>
