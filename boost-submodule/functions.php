7fb374fcb353bd1b6195a23115c20cd549ef1afc
Ageman20XX <adrian.marceau@gmail.com>
2026-08-26 16:59:20 -0400

    Updated Boost Submodule's functions

--- boost-submodule/functions.php
+++ boost-submodule/functions.php
@@ -11,13 +11,11 @@ $functions = array(
     'rpg-ability_stat-boost_before' => function($objects){
         extract($objects);
         if ($this_robot !== $initiator_robot){ return; }
-        if (!$options->is_fixed_amount){
+        if (!$options->is_fixed_amount && $options->boost_amount != 0){
             $trigger_text = $this_robot->print_name().'\'s '.$this_skill->print_name().' doubles stat boosts! ';
             if (!empty($options->extra_text)){ $options->extra_text .= ' <br /> '; }
             $options->extra_text .= $trigger_text;
-            $invert_boost = $options->boost_amount < 0 ? true : false;
             $options->boost_amount *= 2;
-            if ($invert_boost){ $options->boost_amount *= -1; }
         }
         return true;
     }
